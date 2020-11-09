4f3663fe3524b9486143cdd30c09ef02edb69c2b
Alice Horsey <alicehorsey@gmail.com>
2020-11-09 17:54:40 +0000

    seeding the database still not quite working but almost there

--- db/migrations/20201109120328_create_comments.js
+++ db/migrations/20201109120328_create_comments.js
@@ -3,8 +3,8 @@ exports.up = function (knex) {
     console.log('creating comments table...');
     return knex.schema.createTable('comments', (commentsTable) => {
         commentsTable.increments('comment_id').primary();
-        commentsTable.text('author').references('users.username');
-        commentsTable.integer('article_id').references('articles.article_id');
+        commentsTable.text('author').references('username').inTable('users');
+        commentsTable.integer('article_id').references('article_id').inTable('articles');
         commentsTable.integer('votes').defaultTo(0);
         commentsTable.timestamp('created_at');
         commentsTable.text('body');
