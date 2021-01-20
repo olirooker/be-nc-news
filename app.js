3312a819d59e5623ad747ce119f3e5c84815dc55
Oli Rooker <oliver.rooker@gmail.com>
2021-01-20 11:01:14 +0000

    test commit after repo change

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-const express = require("express");
+const express = require('express');
 const cors = require('cors');
 const app = express();
-const apiRouter = require("./routers/api_router");
-const { send404, handleServerErrors, handlePSQLErrors, handleCustomErrors } = require('./controllers/error_controller');
+const apiRouter = require('./routers/api_router');
+const {
+  send404,
+  handleServerErrors,
+  handlePSQLErrors,
+  handleCustomErrors,
+} = require('./controllers/error_controller');
 
 app.use(cors());
 app.use(express.static('public'));
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api', apiRouter)
+app.use('/api', apiRouter);
+app.all('/*', send404);
 
-app.all('/*', send404)
-
-app.use(handleCustomErrors)
-app.use(handlePSQLErrors)
-app.use(handleServerErrors)
+app.use(handleCustomErrors);
+app.use(handlePSQLErrors);
+app.use(handleServerErrors);
 
 module.exports = app;
