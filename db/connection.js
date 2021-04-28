f6a4c1964c46ccefd492bc3d0ed9c41008c9b1df
Oli Rooker <oliver.rooker@gmail.com>
2021-04-28 13:08:21 +0100

    change connection file structure

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,25 @@
+// const knex = require('knex');
+// const dbConfig =
+//     process.env.NODE_ENV === 'production'
+//         ? { client: 'pg', connection: process.env.DATABASE_URL }
+//         : require('../knexfile');
+
+// const connection = knex(dbConfig);
+// module.exports = connection;
+
+const ENV = process.env.NODE_ENV || 'development';
 const knex = require('knex');
 const dbConfig =
-    process.env.NODE_ENV === 'production'
-        ? { client: 'pg', connection: process.env.DATABASE_URL }
-        : require('../knexfile');
-
+  ENV === 'production'
+    ? {
+        client: 'pg',
+        connection: {
+          connectionString: process.env.DATABASE_URL,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
+      }
+    : require('../knexfile');
 const connection = knex(dbConfig);
 module.exports = connection;
