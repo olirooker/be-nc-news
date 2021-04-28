0684d2247f1271c35389314c757a48dec732cc21
Oli Rooker <oliver.rooker@gmail.com>
2021-04-28 12:45:51 +0100

    prettier extension changes

--- routers/api_router.js
+++ routers/api_router.js
@@ -1,18 +1,16 @@
 const apiRouter = require('express').Router();
-const topicsRouter = require('./topics_router')
-const usersRouter = require('./users_router')
-const articlesRouter = require('./articles_router')
-const commentsRouter = require('./comments_router')
-const { getAllEndpoints } = require('../controllers/api_controller')
-const { send405 } = require('../controllers/error_controller')
+const topicsRouter = require('./topics_router');
+const usersRouter = require('./users_router');
+const articlesRouter = require('./articles_router');
+const commentsRouter = require('./comments_router');
+const { getAllEndpoints } = require('../controllers/api_controller');
+const { send405 } = require('../controllers/error_controller');
 
-apiRouter.route('/')
-    .get(getAllEndpoints)
-    .all(send405)
+apiRouter.route('/').get(getAllEndpoints).all(send405);
 
-apiRouter.use('/topics', topicsRouter)
-apiRouter.use('/users', usersRouter)
-apiRouter.use('/articles', articlesRouter)
-apiRouter.use('/comments', commentsRouter)
+apiRouter.use('/topics', topicsRouter);
+apiRouter.use('/users', usersRouter);
+apiRouter.use('/articles', articlesRouter);
+apiRouter.use('/comments', commentsRouter);
 
-module.exports = apiRouter
+module.exports = apiRouter;
